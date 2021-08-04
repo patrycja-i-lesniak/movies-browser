fc600991fc2cfbb1a399cc3f3ae2ec50589757f8
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-04 17:56:00 +0200

    add and style Navigation component

---
+++ src/common/Navigation/index.js
@@ -0,0 +1,24 @@
+import { Container, NavigationContainer, SearchContainer, HeaderContainer } from "./styled";
+import { NavigationLink } from "./NavigationLink";
+import { Header } from "./Header";
+import { Search } from "./Search";
+import {Wrapper } from "../Wrapper/";
+
+export const Navigation = () => {
+
+    return (
+        <NavigationContainer>
+            <Wrapper>
+            <Container>
+                <HeaderContainer>
+                    <Header />
+                    <NavigationLink />
+                </HeaderContainer>
+                <SearchContainer>
+                    <Search />
+                </SearchContainer>
+            </Container>
+            </Wrapper>
+        </NavigationContainer>
+    )
+}
