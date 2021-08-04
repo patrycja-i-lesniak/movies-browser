557f9263e08aec8f5265ef83b991663dbeadb298
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-04 15:30:39 +0200

    styled.js added to Error

---
+++ src/common/Error/styled.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Title = styled.h1`
+    margin: 24px 0;
+    font-size: 36px;
+    line-height: 43px;
+`;
+
+export const Subtitle = styled.h2`
+    margin: 0;
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 29px;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+    margin-top: 24px;
+    padding: 16px 24px;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.colors.button.background};
+    color: ${({ theme }) => theme.colors.button.text};
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 19px;
+    font-family: 'Open Sans', sans-serif;
+`;
