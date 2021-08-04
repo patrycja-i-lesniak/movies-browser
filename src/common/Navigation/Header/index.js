0cd9fd7c52388815e729b702882e1811d7d40481
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-04 17:56:54 +0200

    simplify Header component

--- src/common/Navigation/Header/index.js
+++ src/common/Navigation/Header/index.js
@@ -1,30 +1,16 @@
 import {
-    Container,
-    Navigation,
     Logo,
     Icon,
     PageTitle,
-    NavigationLink,
 } from "./styled";
-import { Search } from "./Search";
-import { Wrapper } from "../../Wrapper";
 import videoIcon from "./videoIcon.svg";
 
 export const Header = () => {
 
     return (
-        <Container>
-            <Wrapper>
-                <Navigation>
-                    <Logo>
-                        <Icon src={videoIcon} />
-                        <PageTitle>Movies Browser</PageTitle>
-                    </Logo>
-                    <NavigationLink>Movies</NavigationLink>
-                    <NavigationLink>People</NavigationLink>
-                    <Search></Search>
-                </Navigation>
-            </Wrapper>
-        </Container>
+            <Logo>
+                <Icon src={videoIcon} />
+                <PageTitle>Movies Browser</PageTitle>
+            </Logo>
     )
 }
