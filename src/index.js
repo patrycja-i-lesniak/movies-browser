d37d899e38d57c512ee7c320e38337228f48e74e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-04 11:09:23 +0200

    change path to App in ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { App } from '../src/core/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -15,3 +14,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
