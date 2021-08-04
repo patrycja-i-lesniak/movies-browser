d32f0c1e8a27ae7f9a12c8dedc81602df8cb9b73
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-04 18:13:11 +0200

    simplify Header component

--- src/common/Navigation/Header/styled.js
+++ src/common/Navigation/Header/styled.js
@@ -1,26 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    background-color: ${({ theme }) => theme.colors.navigation.background};
-    min-height: 94px;
-    width: 100%;
-`;
-
-export const Navigation = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 12px;
-`;
-
 export const Logo = styled.div`
     display: flex;
     align-items: center;
 `;
 
 export const Icon = styled.img`
-    width: 40px;
+    width: 34px;
 `;
 
 export const PageTitle = styled.h1`
@@ -32,16 +18,4 @@ export const PageTitle = styled.h1`
     margin-left: 16px;
 `;
 
-export const NavigationLink = styled.h2`
-    font-weight: 600;
-    font-size: 14px;
-    text-transform: uppercase;
-    color: ${({ theme }) => theme.colors.navigation.elems};
 
-&:active {
-    border: 1px solid #FFFFFF;
-    box-sizing: border-box;
-    border-radius: 24px;
-    padding: 16px 24px;
-}
-`;
