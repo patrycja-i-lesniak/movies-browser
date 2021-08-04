7ec6956250ea9859851cb3fe7f0792e5b4af1af4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-04 17:57:28 +0200

    add and style Navigation component

---
+++ src/common/Navigation/NavigationLink/index.js
@@ -0,0 +1,13 @@
+import { Link } from "./styled";
+
+export const NavigationLink = () => {
+    return (
+        <>
+            <Link>Movies</Link>
+            <Link>People</Link>
+        </>
+    )
+}
+
+
+
