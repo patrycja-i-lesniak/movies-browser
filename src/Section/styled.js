b3882144b56c578657f4955850aa7406e80d4f49
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-05 17:49:49 +0200

    Section styling update

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,42 +1,57 @@
 import styled from "styled-components";
 
-export const SectionWrapper = styled.section`
-    padding: 16px;
-    max-width: 1368px;
-    margin: 0 auto;
-`;
-
 export const PeopleList = styled.ul`
     display: grid;
     grid-gap: 24px;
     grid-template-columns: repeat(6, 1fr);
     list-style-type: none;
     padding: 0;
+
+    @media(max-width: ${({theme}) => theme.breakpoints.large}px) {
+        grid-template-columns: repeat(4, 1fr);
+    }
+    
+    @media(max-width: ${({theme}) => theme.breakpoints.small}px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
 `;
 
 export const PersonTile = styled.li`
     display: grid;
     grid-row-gap: 8px;
+    align-content: start;
     padding: 16px;
-    background-color: #FFFFFF;
+    background-color: ${({theme}) => theme.colors.tile.background};
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
     border-radius: 5px;
-    word-break: break-word;
 `;
 
 export const ProfilePicture = styled.img`
-    background: #C4C4C4;
+    background: ${({theme}) => theme.colors.tile.substitutionPicture};
     border-radius: 5px;
     width: 100%;
 `;
 
 export const TileTitle = styled.h2`
     font-size: 22px;
-    line-height: 29px;
+    line-height: 130%;
     color: ${({theme}) => theme.colors.textPrimary};
     margin: 0;
+    font-weight: 500;
+
+    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+        font-size: 14px;
+    }
 `;
 
 export const Role = styled.p`
     margin: 0;
+    font-size: 18px;
+    line-height: 150%;
+    color: ${({theme}) => theme.colors.tile.role};
+
+    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+        font-size: 13px;
+        line-height: 130%;
+    }
 `;
