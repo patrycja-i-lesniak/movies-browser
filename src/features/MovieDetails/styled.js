306a22bc6d3f34357790b7f3d662a00a25d33845
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-08 00:09:46 +0200

    stylling corr, media querries added

--- src/features/MovieDetails/styled.js
+++ src/features/MovieDetails/styled.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     max-width: 1920px;
-    background: ${({theme}) => theme.colors.site.backgroundWrapper};
-    /* margin: 0 auto; */
+    background: ${({ theme }) => theme.colors.site.backgroundWrapper};
+    padding: 64px;
+
+    @media ( max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    padding: 16px;
+    };
 `;
