0514f4bdfaf491b9982e4c995e67bc54ffb47363
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-08 05:34:08 +0200

    style Rating component, add media queries

--- src/features/Rating/styled.js
+++ src/features/Rating/styled.js
@@ -1,29 +1,50 @@
 import styled from "styled-components";
 
-export const RateingField = styled.div`
+export const RatingField = styled.div`
+grid-area: rating;
     display: grid;
-    grid-template-columns: auto auto auto auto;
+    grid-template-columns: repeat(4, auto);
     justify-content: start;
     align-items: end;
     grid-gap: 8px;
-    margin: 24px 0;
+    margin: 0;
 `;
 
 export const StarIcon = styled.img`
     height: 24px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
+        width: 16px;
+        height: 15.25px;
+    }
 `;
 
-export const RateingScale = styled.p`
+export const Note = styled.p`
+    font-size: 22px;
+    font-weight: 500;
+    margin: 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
+        font-size: 13px;
+    }
+`;
+
+export const RatingScale = styled.p`
     font-size: 14px;
     margin: 0;
 
+    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
+        display: none;
+    }
 `;
 
-export const Votes = styled(RateingScale)`
+export const Votes = styled.p`
     padding-left: 4px;
+    font-size: 14px;
+    margin: 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
+        font-size: 13px;
+    }
 `;
 
-export const Note = styled(RateingScale)`
-    font-size: 22px;
-    font-weight: 500;
-`;
