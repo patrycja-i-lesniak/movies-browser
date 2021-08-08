c6da8f01fbf9e9299c8e60c4a878e99585cff6cf
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-08 15:30:36 +0200

    build Navigation and style its components

---
+++ src/common/Nav/index.js
@@ -0,0 +1,48 @@
+import {
+    NavigationWrapper,
+    NavigationContainer,
+    DoubleContainer,
+    HeaderContainer,
+    Logo,
+    VideoIcon,
+    Title,
+    NavigationLinks,
+    NavigationItems,
+    StyledNavLink,
+    SearchContainer,
+    SearchBox,
+    SearchIcon,
+    SearchInput,
+} from "./styled"
+import videoIcon from "./videoIcon.svg";
+
+export const Navigation = () => {
+    return (
+        <NavigationWrapper>
+            <NavigationContainer>
+                <DoubleContainer>
+                    <HeaderContainer>
+                        <Logo>
+                            <VideoIcon src={videoIcon} />
+                            <Title>Movie Browser</Title>
+                        </Logo>
+                        <NavigationLinks>
+                            <NavigationItems>
+                                <StyledNavLink to="/movies">Movie</StyledNavLink>
+                            </NavigationItems>
+                            <NavigationItems>
+                                <StyledNavLink to="/people">People</StyledNavLink>
+                            </NavigationItems>
+                        </NavigationLinks>
+                    </HeaderContainer>
+                    <SearchContainer>
+                        < SearchBox>
+                            <SearchIcon />
+                            <SearchInput placeholder="Search for movies" />
+                        </SearchBox>
+                    </SearchContainer>
+                </DoubleContainer>
+            </NavigationContainer>
+        </NavigationWrapper>
+    )
+};
---
+++ src/common/Nav/styled.js
@@ -0,0 +1,154 @@
+import styled from "styled-components";
+import searchIcon from "./searchIcon.svg";
+import { NavLink } from "react-router-dom";
+
+export const NavigationWrapper = styled.div`
+    background: ${({ theme }) => theme.colors.navigation.background};
+    color: ${({ theme }) => theme.colors.navigation.elems};
+    min-height: 94px;
+`;
+
+export const NavigationContainer = styled.div`
+    max-width: 1368px;
+    margin: 0 auto;
+    min-height: inherit;
+`;
+
+export const DoubleContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    min-height: inherit;
+    padding: 0 10px;
+`;
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    min-height: inherit;
+    margin: 0 auto;
+    min-height: 54px;
+`;
+
+export const Logo = styled.a`
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+`;
+
+export const VideoIcon = styled.img`
+    color: ${({ theme }) => theme.colors.navigation.elems};
+    width: 34px;
+    margin-right: 12px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
+        width: 26px;
+        margin-left: 20px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xxsmall}px) {
+        width: 14px;
+        margin-left: 20px;
+    }
+`;
+
+export const Title = styled.h1`
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.navigation.elems};
+    font-size: 24px;
+    font-weight: 500;
+    margin: 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
+        font-size: 22px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xxsmall}px) {
+        font-size: 14px;
+    }
+`;
+
+export const NavigationLinks = styled.ul`
+    display: flex;
+    list-style: none;
+    padding-left: 0;
+`;
+
+export const NavigationItems = styled.li`
+`;
+
+const activeClassName = "active";
+
+export const StyledNavLink = styled(NavLink).attrs(() => ({ activeClassName }))`
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 14px;
+    text-decoration: none;
+    padding: 8px 24px;
+
+    &${activeClassName} {
+        border: 1px solid ${({ theme }) => theme.colors.navigation.elems};
+        border-radius: 24px;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xxsmall}px) {
+        font-size: 12px;
+        padding: 8px 12px;
+    }
+`;
+
+export const SearchContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-basis: 50%;
+    min-height: inherit;
+    padding: 0 10px;
+    flex-wrap: wrap;
+    margin: 0 auto;
+    min-height: 74px;
+`;
+
+export const SearchBox = styled.form`
+    position: relative;
+`;
+
+export const SearchIcon = styled.button`
+    position: absolute;
+    left: 24px;
+    top: 25%;
+    width: 24px;
+    height: 24px;
+    background-size: 24px;
+    background-image: url(${searchIcon});
+    background-color: transparent;
+    border: none;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
+        background-size: 16px;
+        width: 16px;
+        width: 16px;
+        left: 16px;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+`;
+
+export const SearchInput = styled.input`
+    width: 432px;
+    max-width: 80vw;
+    height: 48px;
+    border: none;
+    border-radius: 33px;
+    padding: 0;
+    padding-left: 64px;
+    background-color: ${({ theme }) => theme.colors.navigation.elems};
+    outline: none;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
+        padding-left: 44px;
+    }
+`;
+
+
+
