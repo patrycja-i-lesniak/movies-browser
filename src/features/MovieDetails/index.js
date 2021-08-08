cc7993c0702ce3ecc6575723da93b4d27e04c12d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-08 05:32:40 +0200

    delete unnecesary import

--- src/features/MovieDetails/index.js
+++ src/features/MovieDetails/index.js
@@ -1,13 +1,11 @@
 import {
     MovieTile,
-    MobileMovieTile,
 } from "../Tiles";
 
 export const MovieDetails = () => {
     return (
         <>
             <MovieTile />
-            <MobileMovieTile />
         </>
     )
 };
