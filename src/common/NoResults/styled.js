09707408564be022c99634b51bf69d67c957b1f1
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-11 01:22:25 +0200

    not used styling deleted

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -1,8 +1,6 @@
 import styled from "styled-components";
-import { Header, Wrapper } from "../Loader/styled";
+import { Header } from "../Loader/styled";
 import { ReactComponent as Image } from "./NoResultsPicture.svg"
-
-export const NoResultsWrapper = styled(Wrapper)``;
 
 export const NoResultsHeader = styled(Header)`
     margin: 56px 56px 40px;
@@ -10,7 +8,7 @@ export const NoResultsHeader = styled(Header)`
     @media ( max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         margin: 36px 36px 40px;
     };
-    
+
     @media ( max-width: ${({ theme }) => theme.breakpoints.small}px) {
         margin: 0;
     };
@@ -19,7 +17,7 @@ export const NoResultsHeader = styled(Header)`
 export const NoResultsImage = styled(Image)`
     margin: 0 auto;
     width: 668px;
-    height: 508.63px;
+    height: 508px;
 
     @media ( max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         width: 48%;
