c08d6c3f99f4cfedc76db7a1e80a9f3f5f7e5e55
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-11 00:24:44 +0200

    added rating and votesNumber

--- src/features/MovieDetails/Header/HeaderRating/index.js
+++ src/features/MovieDetails/Header/HeaderRating/index.js
@@ -1,17 +1,22 @@
 import star from "../../../Images/star.svg"
 import { HeaderDiv, HeaderNote, HeaderScale, HeaderStarImage, HeaderVotes, Wrapper } from "./styled";
 
-const HeaderRating = () => (
-    <HeaderDiv>
-        <Wrapper>
-            <HeaderStarImage src={star} alt="star" />
-            <div>
-                <HeaderNote>7,8</HeaderNote>
-                <HeaderScale>/ 10</HeaderScale>
-            </div>
-        </Wrapper>
-        <HeaderVotes>335 votes</HeaderVotes>
-    </HeaderDiv>
-);
+const HeaderRating = () => {
+    const rating = 7.8;
+    const votesNumber= 335;
+
+    return (
+        <HeaderDiv>
+            <Wrapper>
+                <HeaderStarImage src={star} alt="star" />
+                <div>
+                    <HeaderNote>{rating}</HeaderNote>
+                    <HeaderScale>/ 10</HeaderScale>
+                </div>
+            </Wrapper>
+            <HeaderVotes>{votesNumber} votes</HeaderVotes>
+        </HeaderDiv>
+    );
+}
 
 export default HeaderRating;
