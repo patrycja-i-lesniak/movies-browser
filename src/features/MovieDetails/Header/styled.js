09d259471b2c8e0dbc9946732d9899c66a5be9c6
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-11 00:24:26 +0200

    styling removing annecessary code

--- src/features/MovieDetails/Header/styled.js
+++ src/features/MovieDetails/Header/styled.js
@@ -1,23 +1,18 @@
 import styled from "styled-components";
 import poster from "./poster.png";
 
+export const MovieDetailsHeader = styled.header`
+    background-color: ${({ theme }) => theme.colors.movieDetails.background};
+`;
+
 export const WrapperPoster = styled.div`
-margin: 0 auto;
     max-width: 1920px;
-    max-height: 769px;
-    height: 50vw;
+    max-height: 679px;
+    margin: 0 auto;
     background-image: url("${poster}");
     background-repeat: no-repeat;
     background-position: center;;
     background-size: cover;
-
-    @media ( max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-        max-height:600px;
-};
-
-    @media ( max-width: ${({ theme }) => theme.breakpoints.small}px) {
-        max-height: 400px;
-    };
 `;
 
 export const Wrapper = styled.div`
@@ -29,33 +24,7 @@ export const Wrapper = styled.div`
     display:flex;
     flex-direction: column;
     justify-content: flex-end;
-    background: linear-gradient(270deg, #000000 14.11%,
-                    rgba(0, 0, 0, 0.873268) 15.08%,
-                    rgba(0, 0, 0, 0.720529) 16.51%,
-                    rgba(0, 0, 0, 0.294577) 19.99%,
-                    rgba(0, 0, 0, 0.159921) 21.88%,
-                    rgba(0, 0, 0, 0) 25.68%),
-                linear-gradient(90deg, #000000 13.6%,
-                    rgba(0, 0, 0, 0.984059) 14.58%,
-                    rgba(0, 0, 0, 0.967732) 15.44%,
-                    rgba(0, 0, 0, 0.865569) 16.3%,
-                    rgba(0, 0, 0, 0.230315) 22.87%,
-                    rgba(0, 0, 0, 0) 26.64%),
-                linear-gradient(180deg, #000000 0%,
-                    rgba(0, 0, 0, 0.689555) 4.66%,
-                    rgba(0, 0, 0, 0.439033) 9.34%,
-                    rgba(0, 0, 0, 0.20628) 15.16%,
-                    rgba(0, 0, 0, 0) 24.22%),
-                linear-gradient(189.44deg,
-                    rgba(0, 0, 0, 0) 58.48%,
-                    rgba(0, 0, 0, 0.106473) 63.17%,
-                    rgba(0, 0, 0, 0.235359) 68.85%,
-                    rgba(0, 0, 0, 0.492821) 78.08%,
-                    rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%);
-
-    @media ( max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-        width: 100%;
-        background: linear-gradient(269.75deg, #000000 8.69%,
+    background: linear-gradient(269.75deg, #000000 8.69%,
                         rgba(0, 0, 0, 0.873268) 10.09%,
                         rgba(0, 0, 0, 0.720529) 12.16%,
                         rgba(0, 0, 0, 0.294577) 17.19%,
@@ -78,14 +47,13 @@ export const Wrapper = styled.div`
                         rgba(0, 0, 0, 0.235359) 74.4%,
                         rgba(0, 0, 0, 0.492821) 82.77%,
                         rgba(0, 0, 0, 0.740286) 89.82%, #000000 96.18%);
-    };
 `;
 
 export const TextWrapper = styled.article`
     max-width: 1368px;
-    width: 100vw;
+    /* width: 100vw; */
     flex-direction: column;
-    align-self: center;
+    /* align-self: center; */
     justify-content: flex-end;
     color: ${({ theme }) => theme.colors.textHeader};
     margin-left: 56px;
@@ -100,11 +68,15 @@ export const TextWrapper = styled.article`
 `;
 
 export const TitleHeader = styled.h1`
-    font-style: normal;
     font-weight: 600;
     font-size: 64px;
-    line-height: 120%;
-    margin-bottom: 24.98px;
+    line-height: 1.2;
+    margin-bottom: 24px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.large}px) {
+        font-size: 48px;
+        margin-bottom: 16px;
+    };
 
     @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         font-size: 32px;
