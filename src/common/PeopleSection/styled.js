322a11cb488bdcaaf6daaa39cdc002ffe87621a9
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-11 10:49:17 +0200

    extra @media in PeopleList added

--- src/common/PeopleSection/styled.js
+++ src/common/PeopleSection/styled.js
@@ -5,7 +5,7 @@ export const Title = styled.h1`
     line-height: 120%;
     margin: 0 0 32px;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         font-size: 20px;
         margin-bottom: 12px;
     }
@@ -18,11 +18,15 @@ export const PeopleList = styled.ul`
     list-style-type: none;
     padding: 0;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.large}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.large}px) {
         grid-template-columns: repeat(4, 1fr);
     }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    grid-template-columns: repeat(3, 1fr);
+    }
     
-    @media(max-width: ${({theme}) => theme.breakpoints.small}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
         grid-template-columns: repeat(2, 1fr);
     }
 `;
@@ -32,13 +36,13 @@ export const PersonTile = styled.li`
     grid-row-gap: 8px;
     align-content: start;
     padding: 16px;
-    background-color: ${({theme}) => theme.colors.tile.background};
+    background-color: ${({ theme }) => theme.colors.tile.background};
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
     border-radius: 5px;
 `;
 
 export const ProfilePicture = styled.img`
-    background: ${({theme}) => theme.colors.tile.substitutionPicture};
+    background: ${({ theme }) => theme.colors.tile.substitutionPicture};
     border-radius: 5px;
     width: 100%;
 `;
@@ -46,11 +50,11 @@ export const ProfilePicture = styled.img`
 export const TileTitle = styled.h2`
     font-size: 22px;
     line-height: 130%;
-    color: ${({theme}) => theme.colors.textPrimary};
+    color: ${({ theme }) => theme.colors.textPrimary};
     margin: 0;
     font-weight: 500;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         font-size: 14px;
     }
 `;
@@ -59,9 +63,9 @@ export const Role = styled.p`
     margin: 0;
     font-size: 18px;
     line-height: 150%;
-    color: ${({theme}) => theme.colors.tile.role};
+    color: ${({ theme }) => theme.colors.tile.role};
 
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         font-size: 13px;
         line-height: 130%;
     }
