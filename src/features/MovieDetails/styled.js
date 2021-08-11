019eb2adc06febe8ca1e0bbd015b4d1e970da141
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-11 21:24:49 +0200

    added max-width

--- src/features/MovieDetails/styled.js
+++ src/features/MovieDetails/styled.js
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
     margin: 0 auto;
     background: ${({ theme }) => theme.colors.site.backgroundWrapper};
     padding: 64px;
+    max-width: 1368px;
 
     @media ( max-width: ${({ theme }) => theme.breakpoints.medium}px) {
     padding: 16px;
