4088ef7473e6ada7c1217919013c687066dea3be
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-17 00:09:09 +0200

    logic closed in MovieDetailsContent

--- src/features/MovieDetails/index.js
+++ src/features/MovieDetails/index.js
@@ -3,27 +3,36 @@ import { Wrapper } from "./styled";
 import { BigMovieTile } from "../Tiles/BigMovieTile";
 import PeopleTiles from "../Tiles/PeopleTiles";
 import { useAPIData } from "./useAPIData";
-import  {Loader} from "../../common/Loader";
+import { Loader } from "../../common/Loader";
 import Error from "../../common/Error";
+import { NoResults } from "../../common/NoResults";
 
 export const MovieDetails = () => {
     const APIData = useAPIData();
-    console.log(APIData);
+
+    const MovieDetailsContent = () => {
+        switch (APIData.status) {
+            case "loading":
+                return < Loader />;
+            case "success":
+                return (
+                    <>
+                        <Header movieData={APIData.movieData.data} />
+                        <Wrapper>
+                            <BigMovieTile movieData={APIData.movieData.data} />
+                            <PeopleTiles title="cast" creditsData={APIData.creditsData.data} />
+                            <PeopleTiles title="crew" creditsData={APIData.creditsData.data} />
+                        </Wrapper>
+                    </>);
+            case "error":
+                return (<Error />);
+            default:
+                <NoResults />;
+                break;
+        };
+    };
 
     return (
-        <>
-            {APIData.status === "loading" && <Loader />}
-            {APIData.status === "success" &&
-                <>
-                    <Header movieData={APIData.movieData.data}/>
-                    <Wrapper>
-                        <BigMovieTile movieData={APIData.movieData.data}/>
-                        <PeopleTiles title="cast" />
-                        <PeopleTiles title="crew" />
-                    </Wrapper>
-                </>
-            }
-            {APIData.status === "error" && <Error />}
-        </>
+        <MovieDetailsContent />
     );
 };
