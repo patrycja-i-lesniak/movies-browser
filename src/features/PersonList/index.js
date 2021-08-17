858bb65e4fb8de974a9226d45358c6a822b8ebac
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-18 01:50:15 +0200

    PersonList prov. with api data, missing pagination

--- src/features/PersonList/index.js
+++ src/features/PersonList/index.js
@@ -1,5 +1,32 @@
+import PeopleTiles from "../Tiles/PeopleTiles";
+import { useAPIData } from "../../useAPIData";
+import { Loader } from "../../common/Loader";
+import Error from "../../common/Error";
+import { NoResults } from "../../common/NoResults";
+import { Wrapper } from "../MovieDetails/styled";
+
 export const PersonList = () => {
+    const APIData = useAPIData();
+
+    const PersonListContent = () => {
+        switch (APIData.status) {
+            case "loading":
+                return < Loader />;
+            case "success":
+                return (
+                    <>
+                        <Wrapper>
+                            <PeopleTiles title="Popular People" popularPeopleData={APIData.popularPeopleData.data} />
+                        </Wrapper>
+                    </>);
+            case "error":
+                return <Error />;
+            default:
+                return <NoResults />;
+        };
+    };
+
     return (
-        <div>Popular People</div>
+        <PersonListContent />
     );
 };
