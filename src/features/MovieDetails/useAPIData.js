9277f19fa3e03a96c8eb5d5afbc149188ab88db6
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-17 23:23:40 +0200

    corrected quotes and names in getAPIData

--- src/features/MovieDetails/useAPIData.js
+++ src/features/MovieDetails/useAPIData.js
@@ -10,20 +10,20 @@ export const useAPIData = () => {
     useEffect(() => {
         const getAPIData = async () => {
             try {
-                const APIKey = 'api_key=3af561a12389e6d632bf79207cb88b6c';
-                const APIAdress = 'https://api.themoviedb.org/3/';
-                const movieId = '21';
+                const APIKey = "api_key=3af561a12389e6d632bf79207cb88b6c";
+                const APIAdress = "https://api.themoviedb.org/3/";
+                const movieId = "21";
 
                 const movieDetailsAPI = `${APIAdress}movie/${movieId}?${APIKey}`;
-                const response = await axios.get(movieDetailsAPI);
+                const movieData = await axios.get(movieDetailsAPI);
 
                 const creditsAPI = `${APIAdress}movie/${movieId}/credits?${APIKey}`;
-                const response2 = await axios.get(creditsAPI);
+                const creditsData= await axios.get(creditsAPI);
 
                 setMovieData({
                     status: "success",
-                    movieData: response,
-                    creditsData: response2
+                    movieData,
+                    creditsData
                 });
 
             } catch (error) {
