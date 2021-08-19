9962ca7149489c8dac749ef345a1850a59034690
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-19 18:22:23 +0200

    remove Arrow to separate component, change paths

--- src/features/Tiles/Tile/Arrow/styled.js
+++
@@ -1,7 +0,0 @@
-import styled, { css } from "styled-components";
-
-export const StyledArrow = styled.svg`
-    ${({ showMore }) => showMore && css`
-        transform: rotate(180deg);  
-    `}
-`;
