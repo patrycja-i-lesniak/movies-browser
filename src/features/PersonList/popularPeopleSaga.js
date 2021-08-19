24d37316eb8c6d0c68eb2229d7eaa093a2b39469
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-20 00:22:43 +0200

    deleted delay in popularPeopleSaga

--- src/features/PersonList/popularPeopleSaga.js
+++ src/features/PersonList/popularPeopleSaga.js
@@ -1,10 +1,9 @@
-import { takeEvery, delay, call, put } from "@redux-saga/core/effects";
+import { takeEvery, call, put } from "@redux-saga/core/effects";
 import { getPopularPeopleData } from "./popularPeopleApi";
 import { fetchPopularPeopleError, fetchPopularPeopleLoading, fetchPopularPeopleSuccess } from "./popularPeopleSlice";
 
 function* fetchPopularPeopleDataHandler({ payload: page }) {
     try {
-        yield delay(2_000);
         const popularPeopleData = yield call(getPopularPeopleData, page);
         yield put(fetchPopularPeopleSuccess(popularPeopleData));
     } catch (error) {
