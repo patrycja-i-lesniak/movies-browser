d47f1dde971e6fc183077e2d278b58d5384b9e01
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-19 15:24:12 +0200

    add MovieList with API

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -1,5 +1,38 @@
-export const MovieList = () => {
+import MovieTiles from "../Tiles/MovieTiles";
+import { useAPIData } from "../../useAPIData";
+import { Loader } from "../../common/Loader";
+import Error from "../../common/Error";
+import { NoResults } from "../../common/NoResults";
+import { Wrapper } from "../MovieDetails/styled";
+// import { Pagination } from "../../common/Pagination";
+
+const MovieList = () => {
+    const APIData = useAPIData();
+
+    const MovieListContent = () => {
+        switch (APIData.status) {
+            case "loading":
+                return < Loader />;
+            case "success":
+                return (
+                    <>
+                        <Wrapper>
+                            <MovieTiles title="Popular Movie"
+                                popularMoviesData={APIData.popularMoviesData.data}
+                            />
+                        </Wrapper>
+                        {/* <Pagination popularMovieData={APIData.popularMoviesData.data}/> */}
+                    </>);
+            case "error":
+                return <Error />;
+            default:
+                return <NoResults />;
+        };
+    };
+
     return (
-        <div>Popular Movies</div>
+        <MovieListContent />
     );
 };
+
+export default MovieList;
