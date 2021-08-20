68af05ef8692fcb8e94df365e2fa97e6ac904790
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-20 08:58:50 +0200

    change @media for List

--- src/features/Tiles/MovieTiles/styled.js
+++ src/features/Tiles/MovieTiles/styled.js
@@ -24,19 +24,16 @@ export const List = styled.ul`
     padding: 0;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.large}px) {
-        grid-template-columns: repeat(4, 1fr);
-    }
-
-    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         grid-template-columns: repeat(3, 1fr);
     }
 
-    @media(max-width: ${({ theme }) => theme.breakpoints.small}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         grid-template-columns: repeat(2, 1fr);
     }
 
     @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         grid-template-columns: 1fr;
+        grid-gap: 20px;
     }
 `;
 
