e92ef986a84265baff57d9db03562300a048222d
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-20 23:42:10 +0200

    added getPopularMoviesGenresData

--- src/features/MovieList/popularMoviesAPI.js
+++ src/features/MovieList/popularMoviesAPI.js
@@ -6,3 +6,7 @@ import { pathAPI } from "../../pathAPI";
 export const getPopularMoviesData = page =>
     axios.get(`${pathAPI}movie/popular?${APIKey}&language=en-US&page=${page}`)
         .then(response => response.data);
+
+export const getPopularMoviesGenresData = () =>
+    axios.get(`${pathAPI}genre / movie / list?${APIKey}&language=en-US`)
+        .then(response => response.data);
