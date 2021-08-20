bfdc970cc5e0fcbaf03bdfdc03dee211d7ce7768
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-20 15:52:10 +0200

    popularMoviesSaga added

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,8 +1,10 @@
 import { all } from "@redux-saga/core/effects";
+import { popularMoviesSaga } from "../features/MovieList/popularPeopleSaga";
 import { popularPeopleSaga } from "../features/PersonList/popularPeopleSaga";
 
 export default function* saga() {
     yield all([
         popularPeopleSaga(),
+        popularMoviesSaga(),
     ]);
 }
