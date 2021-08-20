e2ea28a122868680f714d9d76501081ce494506e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-20 09:12:05 +0200

    change poster size,format code

--- src/features/Tiles/MovieTiles/index.js
+++ src/features/Tiles/MovieTiles/index.js
@@ -29,29 +29,28 @@ const MovieTiles = ({ title, popularMoviesData, movieData }) => {
                         <Tile
                             to={`/movie/${movie.id}`}
                         >
-                            
                             <Picture
-                                src={movie.poster_path ? `https://image.tmdb.org/t/p/w185${movie.poster_path}`
+                                src={movie.poster_path ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
                                     : pictureSubstitution
                                 }
                                 alt={`${title} poster`}
                             />
                             <ContentContainer>
-                            <TileTitle>{movie.title}</TileTitle>
-                            {movie.release_date && (
-                                <Year>
-                                    {movie.release_date.slice(0, 4)}
-                                </Year>
-                            )}
+                                <TileTitle>{movie.title}</TileTitle>
+                                {movie.release_date && (
+                                    <Year>
+                                        {movie.release_date.slice(0, 4)}
+                                    </Year>
+                                )}
 
-                            <Tags>
-                                {genres.map(tag => <Tag key={`genres-${tag.name}`}>{tag.name}</Tag>)}
-                            </Tags>
-                            <Rate
-                                small={true}
-                                vote_average={movie.vote_average}
-                                vote_count={movie.vote_count} />
-                                </ContentContainer>
+                                <Tags>
+                                    {genres.map(tag => <Tag key={`genres-${tag.name}`}>{tag.name}</Tag>)}
+                                </Tags>
+                                <Rate
+                                    small={true}
+                                    vote_average={movie.vote_average}
+                                    vote_count={movie.vote_count} />
+                            </ContentContainer>
                         </Tile>
                     </li>
                 )}
