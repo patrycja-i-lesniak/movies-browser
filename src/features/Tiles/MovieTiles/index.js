c2f0a3e777a9955c618d07e69f701c1adab213dd
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-20 06:53:32 +0200

    delete button change props in Rate add a condition

--- src/features/Tiles/MovieTiles/index.js
+++ src/features/Tiles/MovieTiles/index.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { Arrow } from "../Arrow";
 import pictureSubstitution from "../pictureSubstitution.svg";
 import {
     SiteTitle,
@@ -8,69 +6,52 @@ import {
     Picture,
     TileTitle,
     Year,
-    ShowMoreButton,
     Tags,
     Tag,
 } from "./styled";
-import {Rate} from "../../Rate";
+import { Rate } from "../../Rate";
 
 
 
 const MovieTiles = ({ title, popularMoviesData, movieData }) => {
-    const [showMore, setShowMore] = useState(false);
     const moviesList = popularMoviesData.results;
     const genres = movieData.genres;
     // const year = movieData.release_date.slice(0, 4);
-    const vote_average = movieData.vote_average;
-    const vote_count = movieData.vote_count;
-
 
     return (
         <>
-            <section>
-                <SiteTitle>{title}</SiteTitle>
-                <List>
-                    {moviesList.map((movie, index) =>
-                        <li key={index}>
-                            <Tile
-                                to={`/movie/${movie.id}`}
-                                hidden={
-                                    !showMore &&
-                                    index > 3
+            <SiteTitle>{title}</SiteTitle>
+            <List>
+                {moviesList.map((movie, index) =>
+                    <li key={index}>
+                        <Tile
+                            to={`/movie/${movie.id}`}
+                        >
+                            <Picture
+                                src={movie.poster_path ? `https://image.tmdb.org/t/p/w185${movie.poster_path}`
+                                    : pictureSubstitution
                                 }
-                            >
-                                <Picture
-                                    src={movie.poster_path ? `https://image.tmdb.org/t/p/w185${movie.poster_path}`
-                                        : pictureSubstitution
-                                    }
-                                    alt={`${title} poster`}
-                                />
+                                alt={`${title} poster`}
+                            />
 
-                                <TileTitle>{movie.title}</TileTitle>
+                            <TileTitle>{movie.title}</TileTitle>
+                            {movie.release_date && (
                                 <Year>
                                     {movie.release_date.slice(0, 4)}
                                 </Year>
-                                <Tags>
-                                    {genres.map(tag => <Tag key={`genres-${tag.name}`}>{tag.name}</Tag>)}
-                                </Tags>
-                                <Rate 
+                            )}
+
+                            <Tags>
+                                {genres.map(tag => <Tag key={`genres-${tag.name}`}>{tag.name}</Tag>)}
+                            </Tags>
+                            <Rate
                                 small={true}
-                                vote_average={vote_average}
-                                vote_count={vote_count}/>
-                            </Tile>
-                        </li>
-                    )}
-                </List>
-            </section>
-            {
-                !moviesList.includes("popular") &&
-                <ShowMoreButton onClick={() => setShowMore(!showMore)}>
-                    <span>
-                        {showMore ? "Show less" : "Show more"}
-                    </span>
-                    <Arrow showMore={showMore} />
-                </ShowMoreButton>
-            }
+                                vote_average={movie.vote_average}
+                                vote_count={movie.vote_count} />
+                        </Tile>
+                    </li>
+                )}
+            </List>
         </>
     );
 };
