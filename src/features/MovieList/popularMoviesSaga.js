d1ab65ec90be1107c16b091a33ba1943f173abac
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-21 03:04:34 +0200

    added second API path to saga

--- src/features/MovieList/popularMoviesSaga.js
+++ src/features/MovieList/popularMoviesSaga.js
@@ -1,28 +1,19 @@
-import { takeEvery, delay, call, put , all} from "@redux-saga/core/effects";
+import { takeEvery, call, put, all } from "@redux-saga/core/effects";
+
 import { getGenres, getPopularMoviesData } from "./popularMoviesAPI";
 import { fetchPopularMoviesError, fetchPopularMoviesLoading, fetchPopularMoviesSuccess } from "./popularMoviesSlice";
 
 function* fetchPopularMoviesDataHandler({ payload: page }) {
     try {
-        yield delay(2_000);
-        const popularMoviesData = yield call(getPopularMoviesData, page);
-        yield put(fetchPopularMoviesSuccess(popularMoviesData));
+        const [popularMoviesData, popularMoviesGenres] = yield all([
+            call(getPopularMoviesData, page),
+            call(getGenres)
+        ]);
+        yield put(fetchPopularMoviesSuccess({ popularMoviesData, popularMoviesGenres }));
     } catch (error) {
         yield put(fetchPopularMoviesError());
     }
-};
-
-// function* fetchPopularMoviesDataHandler({ payload: page }) {
-//     try {
-//         const [popularMoviesData, popularMoviesGenres] = yield all([
-//             call(getPopularMoviesData, page),
-//             call(getGenres)
-//         ]);
-//         yield put(fetchPopularMoviesSuccess({ popularMoviesData, popularMoviesGenres }));
-//     } catch (error) {
-//         yield put(fetchPopularMoviesError());
-//     }
-// };
+}
 
 export function* popularMoviesSaga() {
     yield takeEvery(fetchPopularMoviesLoading.type, fetchPopularMoviesDataHandler);
