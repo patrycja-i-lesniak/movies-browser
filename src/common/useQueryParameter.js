1204cf2b9ad0b1d746d72c639cd837be72684e5f
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-21 15:27:51 +0200

    moved useQueryParameter to common

--- src/common/useQueryParameter.js
+++ src/common/useQueryParameter.js
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 
 export const useQueryParameter = (queryParamName) => {
-    const location = useLocation();
+    const { search } = useLocation();
 
-    return (new URLSearchParams(location.search)).get(queryParamName);
+    return (new URLSearchParams(search)).get(queryParamName);
 };
