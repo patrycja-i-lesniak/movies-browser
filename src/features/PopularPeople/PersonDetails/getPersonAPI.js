4b50e96d1fef85849a175182f5b91a07d8db4ecd
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-22 15:47:14 +0200

    add getPersonAPI - credits and data

---
+++ src/features/PopularPeople/PersonDetails/getPersonAPI.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { APIKey, APIPath } from "../../../API";
+
+export const getPersonCredits = personID =>
+    axios.get(`${APIPath}person/${personID}/credits?${APIKey}&language=en-US`)
+        .then(response => response.data);
+
+export const getPersonData = personID =>
+axios.get(`${APIPath}person/${personID}?${APIKey}&language=en-US`)
+    .then(response => response.data);
