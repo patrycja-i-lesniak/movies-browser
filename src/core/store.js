0199960add050096c1f6f7e08cd88d92367b3c01
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-22 15:49:35 +0200

    add personDetailsReducer to store

--- src/core/store.js
+++ src/core/store.js
@@ -3,6 +3,7 @@ import createSagaMiddleware from "@redux-saga/core";
 import popularMoviesReducer from "../features/PopularMovies/popularMoviesSlice";
 import popularPeopleReducer from "../features/PopularPeople/popularPeopleSlice";
 import movieDetailsReducer from "../features/PopularMovies/MovieDetails/movieDetailsSlice";
+import personDetailsReducer from "../features/PopularPeople/PersonDetails/personDetailsSlice";
 import saga from "./saga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -12,6 +13,7 @@ const store = configureStore({
         popularMovies: popularMoviesReducer,
         popularPeople: popularPeopleReducer,
         movieDetails: movieDetailsReducer,
+        personDetails: personDetailsReducer,
     },
     middleware: [sagaMiddleware],
 });
