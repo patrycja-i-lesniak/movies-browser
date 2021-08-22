be5f7cea28781aac2613dcdac4d0e7c4c36f61ea
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-22 15:48:02 +0200

    add personDetailSaga to saga

--- src/core/saga.js
+++ src/core/saga.js
@@ -2,11 +2,13 @@ import { all } from "@redux-saga/core/effects";
 import { movieDetailsSaga } from "../features/PopularMovies/MovieDetails/movieDetailsSaga";
 import { popularMoviesSaga } from "../features/PopularMovies/popularMoviesSaga";
 import { popularPeopleSaga } from "../features/PopularPeople/popularPeopleSaga";
+import { personDetailsSaga } from "../features/PopularPeople/PersonDetails/personDetailsSaga";
 
 export default function* saga() {
     yield all([
         popularPeopleSaga(),
         movieDetailsSaga(),
         popularMoviesSaga(),
+        personDetailsSaga(),
     ]);
 };
