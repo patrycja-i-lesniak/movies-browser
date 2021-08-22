fc5f5f191c9a04bd9805274c308946fbe0c551fa
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-22 11:07:53 +0200

    clean up the API files

--- src/APIKey.js
+++
@@ -1,1 +0,0 @@
-export const APIKey = "api_key=3af561a12389e6d632bf79207cb88b6c";
--- src/features/MovieList/popularMoviesAPI.js
+++ src/features/MovieList/popularMoviesAPI.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-import { APIKey } from "../../APIKey";
-import { pathAPI } from "../../pathAPI";
+import { APIKey, APIPath } from "../../API";
 
 export const getPopularMoviesData = page =>
-    axios.get(`${pathAPI}movie/popular?${APIKey}&language=en-US&page=${page}`)
+    axios.get(`${APIPath}movie/popular?${APIKey}&language=en-US&page=${page}`)
         .then(response => response.data);
 
 export const getGenres = () =>
-    axios.get(`${pathAPI}genre/movie/list?${APIKey}&language=en-US`)
+    axios.get(`${APIPath}genre/movie/list?${APIKey}&language=en-US`)
         .then(response => response.data);
