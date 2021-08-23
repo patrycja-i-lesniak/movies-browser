36b07b09694949c101967fd33b69cfe4bc888f1e
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-23 11:15:46 +0200

    added getSearchMoviesData

--- src/features/MovieList/popularMoviesAPI.js
+++ src/features/MovieList/popularMoviesAPI.js
@@ -7,6 +7,10 @@ export const getPopularMoviesData = page =>
     axios.get(`${pathAPI}movie/popular?${APIKey}&language=en-US&page=${page}`)
         .then(response => response.data);
 
+export const getSearchMoviesData = ({ page, searchQuery }) =>
+    axios.get(`${pathAPI}search/movie?${APIKey}&language=en-US&query=${searchQuery}&page=${page}&include_adult=false`)
+        .then(response => response.data);
+
 export const getGenres = () =>
     axios.get(`${pathAPI}genre/movie/list?${APIKey}&language=en-US`)
         .then(response => response.data);
