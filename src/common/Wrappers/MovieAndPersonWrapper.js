cf8236b15ecd00953334191eb80dd8e4150e54ab
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-08-25 20:22:46 +0200

    change padding to margin

--- src/common/Wrappers/MovieAndPersonWrapper.js
+++ src/common/Wrappers/MovieAndPersonWrapper.js
@@ -4,17 +4,17 @@ export const MovieAndPersonWrapper = styled.div`
     margin: 0 auto;
     background: ${({ theme }) => theme.colors.site.backgroundWrapper};
     max-width: 1368px;
-    padding: 0 16px;
+    margin: 32px 16px;
 
     ${({ movie }) => movie && css`
-        margin: 64px 0;
+        margin: 64px 16px;
     `};
 
     ${({ person }) => person && css`
-        margin: 56px 0;
+        margin: 56px 16px;
     `};
 
     @media ( max-width: ${({ theme }) => theme.breakpoints.small}px) {
-    padding: 16px;
+    margin: 16px 16px;
     };
 `;
