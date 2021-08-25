78455ffbb442c165830b8046dff6032bea8bbb02
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-25 02:49:58 +0200

    deleted getGenres

--- src/features/PopularMovies/popularMoviesSaga.js
+++ src/features/PopularMovies/popularMoviesSaga.js
@@ -1,15 +1,12 @@
 import { takeEvery, call, put, all } from "@redux-saga/core/effects";
 
-import { getGenres, getPopularMoviesData } from "./popularMoviesAPI";
+import { getPopularMoviesData } from "./popularMoviesAPI";
 import { fetchPopularMoviesError, fetchPopularMoviesLoading, fetchPopularMoviesSuccess } from "./popularMoviesSlice";
 
 function* fetchPopularMoviesDataHandler({ payload: page }) {
     try {
-        const [popularMoviesData, popularMoviesGenres] = yield all([
-            call(getPopularMoviesData, page),
-            call(getGenres)
-        ]);
-        yield put(fetchPopularMoviesSuccess({ popularMoviesData, popularMoviesGenres }));
+        const [popularMoviesData] = yield all([call(getPopularMoviesData, page)]);
+        yield put(fetchPopularMoviesSuccess({ popularMoviesData }));
     } catch (error) {
         yield put(fetchPopularMoviesError());
     }
