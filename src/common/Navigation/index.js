8ff9c5828cbe171f9435bd180e0484bf7a3668af
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-25 17:39:20 +0200

    useDispatch moved to App, code edition

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -16,43 +16,31 @@ import {
 } from "./styled";
 import videoIcon from "./videoIcon.svg";
 
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { fetchMoviesBrowserLoading } from "../../core/App/moviesBrowserSlice";
-
-export const Navigation = () => {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(fetchMoviesBrowserLoading());
-    }, [dispatch]);
-
-    return (
-        <NavigationWrapper>
-            <NavigationContainer>
-                <DoubleContainer>
-                    <HeaderContainer>
-                        <Logo to="/movies">
-                            <VideoIcon src={videoIcon} />
-                            <Title>Movie Browser</Title>
-                        </Logo>
-                        <NavigationLinks>
-                            <NavigationItems>
-                                <StyledNavLink to="/movies">Movie</StyledNavLink>
-                            </NavigationItems>
-                            <NavigationItems>
-                                <StyledNavLink to="/people">People</StyledNavLink>
-                            </NavigationItems>
-                        </NavigationLinks>
-                    </HeaderContainer>
-                    <SearchContainer>
-                        <SearchBox>
-                            <SearchIcon />
-                            <SearchInput placeholder="Search for movies" />
-                        </SearchBox>
-                    </SearchContainer>
-                </DoubleContainer>
-            </NavigationContainer>
-        </NavigationWrapper>
-    );
-};
+export const Navigation = () => (
+    <NavigationWrapper>
+        <NavigationContainer>
+            <DoubleContainer>
+                <HeaderContainer>
+                    <Logo to="/movies">
+                        <VideoIcon src={videoIcon} />
+                        <Title>Movie Browser</Title>
+                    </Logo>
+                    <NavigationLinks>
+                        <NavigationItems>
+                            <StyledNavLink to="/movies">Movie</StyledNavLink>
+                        </NavigationItems>
+                        <NavigationItems>
+                            <StyledNavLink to="/people">People</StyledNavLink>
+                        </NavigationItems>
+                    </NavigationLinks>
+                </HeaderContainer>
+                <SearchContainer>
+                    <SearchBox>
+                        <SearchIcon />
+                        <SearchInput placeholder="Search for movies" />
+                    </SearchBox>
+                </SearchContainer>
+            </DoubleContainer>
+        </NavigationContainer>
+    </NavigationWrapper>
+);
