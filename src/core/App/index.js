b1a677adf60fceeb655ed69c70b96bd89a359883
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-25 17:38:56 +0200

    call for Api moved from navigation to App

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -10,31 +10,50 @@ import PopularMovieList from "../../features/PopularMovies";
 import Error from "../../common/Error";
 import { MovieDetails } from "../../features/PopularMovies/MovieDetails";
 import PersonDetails from "../../features/PopularPeople/PersonDetails";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMoviesBrowserLoading } from "./moviesBrowserSlice";
+import { useEffect } from "react";
+import { selectMoviesBrowserStatus } from "./moviesBrowserSlice";
 
 export const App = () => {
-  return (
-    <HashRouter>
-      <Navigation />
-      <Switch>
-        <Route path="/profile/:id">
-          <PersonDetails />
-        </Route>
-        <Route path="/movie/:id">
-          <MovieDetails />
-        </Route>
-        <Route path="/movies">
-          <PopularMovieList />
-        </Route>
-        <Route path="/people">
-          <PopularPeopleList />
-        </Route>
-        <Route path="/error">
-          <Error />
-        </Route>
-        <Route path="/">
-          <Redirect to="/movies" />
-        </Route>
-      </Switch>
-    </HashRouter>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMoviesBrowserLoading());
+  }, [dispatch]);
+
+  const status = useSelector(selectMoviesBrowserStatus);
+
+  switch (status) {
+    case "success":
+      return (
+        <HashRouter>
+          <Navigation />
+          <Switch>
+            <Route path="/profile/:id">
+              <PersonDetails />
+            </Route>
+            <Route path="/movie/:id">
+              <MovieDetails />
+            </Route>
+            <Route path="/movies">
+              <PopularMovieList />
+            </Route>
+            <Route path="/people">
+              <PopularPeopleList />
+            </Route>
+            <Route path="/error">
+              <Error />
+            </Route>
+            <Route path="/">
+              <Redirect to="/movies" />
+            </Route>
+          </Switch>
+        </HashRouter>
+      );
+    case "error":
+      return <Error />;
+    default:
+      return <></>;
+  };
 };
