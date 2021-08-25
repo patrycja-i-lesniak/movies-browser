e23ee2f61e8890cd3fd69d11719aed868b090693
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-25 02:49:44 +0200

    deleted getGenres

--- src/features/PopularMovies/popularMoviesAPI.js
+++ src/features/PopularMovies/popularMoviesAPI.js
@@ -5,7 +5,3 @@ import { APIKey, APIPath } from "../../API";
 export const getPopularMoviesData = page =>
     axios.get(`${APIPath}movie/popular?${APIKey}&language=en-US&page=${page}`)
         .then(response => response.data);
-
-export const getGenres = () =>
-    axios.get(`${APIPath}genre/movie/list?${APIKey}&language=en-US`)
-        .then(response => response.data);
