c62ffb9525a4b3e2d0a0c7e776ba8b90e0c7d1ce
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-25 02:47:19 +0200

    added moviesBrowserSaga to Saga

--- src/core/saga.js
+++ src/core/saga.js
@@ -3,9 +3,11 @@ import { movieDetailsSaga } from "../features/PopularMovies/MovieDetails/movieDe
 import { popularMoviesSaga } from "../features/PopularMovies/popularMoviesSaga";
 import { popularPeopleSaga } from "../features/PopularPeople/popularPeopleSaga";
 import { personDetailsSaga } from "../features/PopularPeople/PersonDetails/personDetailsSaga";
+import { moviesBrowserSaga } from "./App/moviesBrowserSaga";
 
 export default function* saga() {
     yield all([
+        moviesBrowserSaga(),
         popularPeopleSaga(),
         movieDetailsSaga(),
         popularMoviesSaga(),
