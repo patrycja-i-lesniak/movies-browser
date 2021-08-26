582006abbfb2062af267cdd210974694289ed7c3
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-26 16:28:50 +0200

    error handling working

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -1,13 +1,15 @@
 import dangerIcon from "./dangerIcon.svg";
 import { DangerImage, StyledNavLink, Subtitle, Title, Wrapper } from "./styled";
 
-const Error = () => (
-    <Wrapper>
-        <DangerImage src={dangerIcon} alt="" />
-        <Title>Ooops! Something went wrong... </Title>
-        <Subtitle>Please check your network connection and try again</Subtitle>
-        <StyledNavLink to="/">Back to homepage</StyledNavLink>
-    </Wrapper>
-);
+const Error = ({status}) => {
+    return (
+        <Wrapper>
+            <DangerImage src={dangerIcon} alt="" />
+            <Title>Ooops! Something went wrong... </Title>
+            <Subtitle>Please check your network connection and try again</Subtitle>
+            {status === "noButton" ? "" : <StyledNavLink to="/">Back to homepage</StyledNavLink>}
+        </Wrapper>
+    );
+};
 
 export default Error;
