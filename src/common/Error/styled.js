b7879ca1a7903de20588a1fec20cc0682857781b
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-27 00:03:04 +0200

    added hover and active pseudoclasses

--- src/common/Error/styled.js
+++ src/common/Error/styled.js
@@ -7,17 +7,17 @@ export const Wrapper = styled.div`
     text-align: center;
     padding: 16px;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.medium}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         margin-top: 120px;
     }
 
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         margin-top: 80px;
     }
 `;
 
 export const DangerImage = styled.img`
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         width: 80px;
     }
 `;
@@ -27,11 +27,11 @@ export const Title = styled.h1`
     font-size: 36px;
     line-height: 1.2;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.medium}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         font-size: 28px;
     }
 
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         font-size: 20px;
         margin: 16px 0;
     }
@@ -44,11 +44,11 @@ export const Subtitle = styled.h2`
     font-size: 22px;
     line-height: 1.3;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.medium}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         font-size: 18px;
     }
 
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         font-size: 14px;
     }
 `;
@@ -66,7 +66,15 @@ export const StyledNavLink = styled(NavLink)`
     font-family: 'Open Sans', sans-serif;
     text-decoration: none;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.xsmall}px) {
+    &:hover {
+        box-shadow: 2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF;
+    };
+
+    &:active {
+        box-shadow: inset 0px 2px 0px rgba(20, 70, 32, 0.2);
+    };
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         padding: 12px 16px;
         font-size: 12px;
     }
