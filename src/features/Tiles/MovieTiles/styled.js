208823ebc18d74f0113b4b7b2ac84738ea37b764
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-27 01:40:09 +0200

    created one common TilesTile for Movies and People

--- src/features/Tiles/MovieTiles/styled.js
+++ src/features/Tiles/MovieTiles/styled.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-
+import { TilesTile } from "../TilesTile";
 
 export const SiteTitle = styled.h1`
     font-size: 36px;
@@ -34,26 +33,10 @@ export const List = styled.ul`
     }
 `;
 
-export const Tile = styled(Link)`
-    text-decoration: none;
-    display: grid;
+export const Tile = styled(TilesTile)`
     grid-template-rows: auto 1fr;
     grid-row-gap: 16px;
-    align-content: start;
-    padding: 16px;
-    height: 100%;
-    background-color: ${({ theme }) => theme.colors.tile.background};
-    box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-    border-radius: 5px;
     color: inherit;
-
-    &:hover {
-        box-shadow: 0px 4px 12px ${({ theme }) => theme.colors.tile.hoverColor};
-}
-    &:active {
-        border: 1px solid ${({ theme }) => theme.colors.tile.activeColor};
-        box-shadow: none;
-}
 
     @media(max-width: ${({ theme }) => theme.breakpoints.xsmall}px) {
         grid-template-rows: auto;
