4361e059711f80ecee409d0f1c62be557aaa6fec
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-27 00:10:15 +0200

    refactored. added reload or go to homepage options

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -1,13 +1,19 @@
 import dangerIcon from "./dangerIcon.svg";
 import { DangerImage, StyledNavLink, Subtitle, Title, Wrapper } from "./styled";
 
-const Error = ({status}) => {
+const Error = ({ reloadButton }) => {
+    const reloadPage = () => {
+        window.location.reload();
+    };
+
     return (
         <Wrapper>
-            <DangerImage src={dangerIcon} alt="" />
+            <DangerImage src={dangerIcon} alt="error image" />
             <Title>Ooops! Something went wrong... </Title>
             <Subtitle>Please check your network connection and try again</Subtitle>
-            {status === "noButton" ? "" : <StyledNavLink to="/">Back to homepage</StyledNavLink>}
+            {reloadButton
+                ? <StyledNavLink as="button" onClick={reloadPage}>Reload page</StyledNavLink>
+                : <StyledNavLink to="/">Back to homepage</StyledNavLink>}
         </Wrapper>
     );
 };
