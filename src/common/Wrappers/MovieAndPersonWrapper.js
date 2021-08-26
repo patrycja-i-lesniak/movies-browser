e90213f47203dc6b8b6c2497fc71c321742a9887
Wojciech Bylica <83250670+WojciechBylica@users.noreply.github.com>
2021-08-26 08:39:50 +0200

    Stylling correction (#22)
    * MovieAndPersonSection created
    
    * margin 0 auto, rest moved to MovieAndPersonSection
    
    * section changed to StyledSection

--- src/common/Wrappers/MovieAndPersonWrapper.js
+++ src/common/Wrappers/MovieAndPersonWrapper.js
@@ -1,20 +1,7 @@
-import styled, {css} from "styled-components";
+import styled from "styled-components";
 
 export const MovieAndPersonWrapper = styled.div`
     margin: 0 auto;
     background: ${({ theme }) => theme.colors.site.backgroundWrapper};
     max-width: 1368px;
-    margin: 32px 16px;
-
-    ${({ movie }) => movie && css`
-        margin: 64px 16px;
-    `};
-
-    ${({ person }) => person && css`
-        margin: 56px 16px;
-    `};
-
-    @media ( max-width: ${({ theme }) => theme.breakpoints.small}px) {
-    margin: 16px 16px;
-    };
 `;
