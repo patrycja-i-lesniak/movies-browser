8fa0d822159dede0c86f3af31b8e1781f27cf78d
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-26 10:51:03 +0200

    created custom hook useFetchList

---
+++ src/features/useFetchList.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { paginationQueryParamName, searchQueryParamName } from "../common/queryParamNames";
+import { useQueryParameter } from "../common/useQueryParameter";
+
+export const useFetchList = (fetchListLoading) => {
+    const dispatch = useDispatch();
+    const page = useQueryParameter(paginationQueryParamName);
+    const searchQuery = useQueryParameter(searchQueryParamName);
+
+    useEffect(() => {
+        if (searchQuery && !page) {
+            const timeoutID = setTimeout(() => {
+                dispatch(fetchListLoading({ page, searchQuery }));
+            }, 1_000);
+
+            return () => clearTimeout(timeoutID);
+        } else {
+            dispatch(fetchListLoading({ page, searchQuery }));
+        }
+    }, [dispatch, page, searchQuery, fetchListLoading]);
+};
