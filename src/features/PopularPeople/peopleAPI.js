24d8dd74a20201fbef78b0c5e962902b31068a69
Karol Ciesluk <karolciesluk.db@gmail.com>
2021-08-27 17:23:40 +0200

    added peopleAPI.js

---
+++ src/features/PopularPeople/peopleAPI.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { APIKey, APIPath } from "../../API";
+
+export const getPopularPeopleData = page =>
+    axios.get(`${APIPath}person/popular?${APIKey}&language=en-US&page=${page}`)
+        .then(response => response.data);
+
+export const getSearchPeopleData = ({ page, searchQuery }) =>
+    axios.get(`${APIPath}search/person?${APIKey}&language=en-US&query=${searchQuery}&page=${page}&include_adult=false`)
+        .then(response => response.data);
