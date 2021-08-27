3169c4c1fdf5b5f351cd0794d8278ef7f15eacdf
Wojciech Bylica <wojtekbyl@gmail.com>
2021-08-28 00:49:26 +0200

    added matchMedia to MovieHeader

--- src/features/PopularMovies/MovieDetails/Header/index.js
+++ src/features/PopularMovies/MovieDetails/Header/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectConfiguration } from "../../../../core/App/movieBrowserSlice";
 import { selectMovieData } from "../movieDetailsSlice";
@@ -16,8 +17,25 @@ export const Header = () => {
     const movieData = useSelector(selectMovieData);
     const configuration = useSelector(selectConfiguration);
 
+    const [mQuery, setMQuery] = useState({
+        matches: window.innerWidth > 1280 ? true : false
+    });
+
+    useEffect(() => {
+        let mediaQuery = window.matchMedia("(min-width: 1280px)");
+        mediaQuery.addListener(setMQuery);
+        return () => mediaQuery.removeListener(setMQuery);
+      }, [mQuery]);
+
+      let size;
+
+      if (mQuery.matches) {
+        size = configuration.images.backdrop_sizes[3];
+      } else {
+        size = configuration.images.backdrop_sizes[2];
+      }
+
     const imageURL = configuration.images.base_url;
-    const size = configuration.images.backdrop_sizes[3];
     const poster = movieData.backdrop_path;
     const posterAdress = `${imageURL}${size}${poster}`;
     const title = movieData.title;
