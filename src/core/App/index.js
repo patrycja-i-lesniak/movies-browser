3e4a8f3cbf45b03db0ebcb696f909c95e176e809
Wojciech Bylica <wojtekbyl@gmail.com>
2021-09-01 00:47:24 +0200

    added welcomeLoader.

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -22,7 +22,8 @@ import { PeopleList } from "../../features/PopularPeople";
 import { Footer } from "../../../src/common/Footer";
 import { selectIsDarkTheme } from "../../common/ThemeSwitch/themeSlice";
 import { ThemeSwitch } from "../../common/ThemeSwitch";
-
+import { WelcomeImage, WelcomeLoader } from "./welcomeLoader.js";
+import  favicon  from "../../features/Images/favicon.svg";
 export const App = () => {
   const dispatch = useDispatch();
 
@@ -36,25 +37,38 @@ export const App = () => {
 
   const AppContent = () => {
     switch (status) {
+      case "loading":
+        return (
+          <WelcomeLoader>
+            <WelcomeImage src={favicon} alt="welcome loading image" />
+          </WelcomeLoader>
+        );
       case "success":
         return (
-          <Switch>
-            <Route path={toProfile()}>
-              <PersonDetails />
-            </Route>
-            <Route path={toMovie()}>
-              <MovieDetails />
-            </Route>
-            <Route path={toMovies()}>
-              <MovieList />
-            </Route>
-            <Route path={toPeople()}>
-              <PeopleList />
-            </Route>
-            <Route path="/">
-              <Redirect to={toMovies()} />
-            </Route>
-          </Switch>);
+          <>
+            <Navigation />
+            <Switch>
+              <Route path={toProfile()}>
+                <PersonDetails />
+              </Route>
+              <Route path={toMovie()}>
+                <MovieDetails />
+              </Route>
+              <Route path={toMovies()}>
+                <MovieList />
+              </Route>
+              <Route path={toPeople()}>
+                <PeopleList />
+              </Route>
+              <Route path="/">
+                <Redirect to={toMovies()} />
+              </Route>
+            </Switch>
+            <Footer>
+              <p>© 2021 | Coded by Wojciech Bylica, Karol Cieśluk, Patrycja Leśniak</p>
+              <ThemeSwitch />
+            </Footer>
+          </>);
       case "error":
         return <Error reloadButton />;
       default:
@@ -67,12 +81,7 @@ export const App = () => {
       <ThemeProvider theme={isDarkTheme ? darkTheme : theme}>
         <Normalize />
         <GlobalStyle />
-        <Navigation />
         <AppContent />
-        <Footer>
-          <p>© 2021 | Coded by Wojciech Bylica, Karol Cieśluk, Patrycja Leśniak</p>
-          <ThemeSwitch />
-        </Footer>
       </ThemeProvider>
     </HashRouter>
 
